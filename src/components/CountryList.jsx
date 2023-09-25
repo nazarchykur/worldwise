9e4b5880bcf35c710f0ef71d0a06cb836a1264f3
nazarchykur <nazarchykur@gmail.com>
2023-09-25 16:30:23 +0300

    added an example of Consuming the CitiesContext

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,10 +1,13 @@
 import Spinner from "./Spinner";
 import CountryItem from "./CountryItem";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
 import styles from "./CountryList.module.css";
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) {
     return <Spinner />;
   }
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const BASE_URL = "http://localhost:5000";
 
@@ -32,4 +32,12 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider };
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined) {
+    throw new Error("useCities must be used within a CitiesProvider");
+  }
+  return context;
+}
+
+export { CitiesProvider, useCities };
